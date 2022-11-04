498e32b70e21ade114b1d22d99a59975875bab3e
Liv Nelson <livnelson@me.com>
2022-11-04 14:02:04 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const array = ["bob", "sally", "tom"]
+console.log(array[0])
+
+function writeCards(array, string){
+    // const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    array[i]=(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return array;
+}
+
+console.log(writeCards(array, "birthday"))
+
+function countDown() {
+    let count = 11;
+    while(count > 0){
+        count--;
+        console.log(count);
+    }
+}
